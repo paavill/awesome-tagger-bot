7bb2f633e56095d5e0c4e8dc080f13f95bcbe238
paavill <pavelchist2001@gmail.com>
2024-08-30 21:01:00 +0300

    rewrite get page

--- get_news.py
+++ get_news.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import time
 
-service = Service('./geckodriver') 
+service = Service('./geckodriver')
 
 options = webdriver.FirefoxOptions()
 
@@ -16,8 +20,14 @@ driver = webdriver.Firefox(service=service, options=options)
 
 try:
     driver.get('https://kakoysegodnyaprazdnik.ru')
-    time.sleep(30)
-    driver.get('https://kakoysegodnyaprazdnik.ru')
+    print("waiting")
+    submit_button = WebDriverWait(driver, 600).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "input[type='submit']"))
+    )
+    submit_button.click()
+    element = WebDriverWait(driver, 600).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "mainpage"))
+    )
     page_html = driver.page_source
     print(page_html)
 finally:
