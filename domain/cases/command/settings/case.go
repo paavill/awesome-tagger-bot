3f6aa0d2b1eb2636455029661e711bc19c53b1a9
paavill <pavelchist2001@gmail.com>
2024-09-04 14:15:01 +0300

    fix

--- domain/cases/command/settings/case.go
+++ domain/cases/command/settings/case.go
@@ -14,17 +14,18 @@ import (
 )
 
 var (
-	mux           = &sync.Mutex{}
-	nul           = "nil"
-	root          = "root"
-	hours         = "hours"
-	minutes       = "minutes"
-	settingsToday = "settings_today"
-	selectHour    = "select_hour"
-	selectMinute  = "select_minute"
-	save          = "save"
-	onOffChange   = "on_off_change"
-	back          = "back"
+	mux                 = &sync.Mutex{}
+	usersCurrentSetting = map[string]*chatHourMinute{}
+	nul                 = "nil"
+	root                = "root"
+	hours               = "hours"
+	minutes             = "minutes"
+	settingsToday       = "settings_today"
+	selectHour          = "select_hour"
+	selectMinute        = "select_minute"
+	save                = "save"
+	onOffChange         = "on_off_change"
+	back                = "back"
 
 	hoursArray   = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"}
 	minutesArray = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59"}
@@ -109,6 +110,11 @@ var (
 	}
 )
 
+type chatHourMinute struct {
+	hour   string
+	minute string
+}
+
 func init() {
 	for i, v := range hoursArray {
 		subArrayIndex := i / 6
@@ -161,6 +167,7 @@ func ProcessCallBack(chatId int64, callbackQuery *tgbotapi.CallbackQuery) {
 	}
 
 	messageId := message.MessageID
+	userId := message.Chat.UserName
 
 	switch data {
 	case selectHour:
@@ -206,6 +213,10 @@ func ProcessCallBack(chatId int64, callbackQuery *tgbotapi.CallbackQuery) {
 			Text: "Настройки",
 		})
 		sendMarkupUpdate(chatId, messageId, &v, callbackQuery.ID)
+
+		mux.Lock()
+		defer mux.Unlock()
+		delete(usersCurrentSetting, userId)
 	case back:
 		v := markUps[root]
 		bot.Bot.Send(tgbotapi.EditMessageTextConfig{
@@ -216,6 +227,10 @@ func ProcessCallBack(chatId int64, callbackQuery *tgbotapi.CallbackQuery) {
 			Text: "Настройки",
 		})
 		sendMarkupUpdate(chatId, messageId, &v, callbackQuery.ID)
+
+		mux.Lock()
+		defer mux.Unlock()
+		delete(usersCurrentSetting, userId)
 	case onOffChange:
 		markup := message.ReplyMarkup
 		m := markup.InlineKeyboard[1][0].Text
@@ -269,10 +284,23 @@ func ProcessCallBack(chatId int64, callbackQuery *tgbotapi.CallbackQuery) {
 
 				mux.Lock()
 				defer mux.Unlock()
+
 				oldHour := markup.InlineKeyboard[0][0].Text
 				oldMinute := markup.InlineKeyboard[0][1].Text
 
-				markup.InlineKeyboard[0][0].Text = v
+				if val, ok := usersCurrentSetting[userId]; ok {
+					val.hour = v
+				} else {
+					usersCurrentSetting[userId] = &chatHourMinute{
+						hour:   v,
+						minute: oldMinute,
+					}
+				}
+
+				setting := usersCurrentSetting[userId]
+				markup.InlineKeyboard[0][0].Text = setting.hour
+				markup.InlineKeyboard[0][1].Text = setting.minute
+
 				sendDetailInfo(chatId, messageId)
 				sendMarkupUpdate(chatId, messageId, &markup, callbackQuery.ID)
 
@@ -288,10 +316,23 @@ func ProcessCallBack(chatId int64, callbackQuery *tgbotapi.CallbackQuery) {
 
 				mux.Lock()
 				defer mux.Unlock()
+
 				oldHour := markup.InlineKeyboard[0][0].Text
 				oldMinute := markup.InlineKeyboard[0][1].Text
 
-				markup.InlineKeyboard[0][1].Text = v
+				if val, ok := usersCurrentSetting[userId]; ok {
+					val.minute = v
+				} else {
+					usersCurrentSetting[userId] = &chatHourMinute{
+						hour:   oldHour,
+						minute: v,
+					}
+				}
+
+				setting := usersCurrentSetting[userId]
+				markup.InlineKeyboard[0][0].Text = setting.hour
+				markup.InlineKeyboard[0][1].Text = setting.minute
+
 				sendDetailInfo(chatId, messageId)
 				sendMarkupUpdate(chatId, messageId, &markup, callbackQuery.ID)
 
