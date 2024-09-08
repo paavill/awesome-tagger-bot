a7c72c6b0033d5aa6cf42d9157d2e6edca091624
paavill <pavelchist2001@gmail.com>
2024-09-08 23:04:02 +0300

    improve get_news.py

--- Dockerfile
+++ Dockerfile
@@ -16,9 +16,12 @@ RUN apt install -y firefox
 RUN apt install -y wget
 RUN apt install -y unzip
 RUN apt install -y python3-pip
+RUN apt install -y vim
 RUN pip install selenium
 RUN wget https://github.com/mozilla/geckodriver/releases/download/v0.35.0/geckodriver-v0.35.0-linux32.tar.gz
 RUN tar -xvzf geckodriver-v0.35.0-linux32.tar.gz
+RUN rm geckodriver-v0.35.0-linux32.tar.gz
+RUN mkdir ./logs
 
 COPY --from=builder /usr/tagger/tagger /bin/tagger
 COPY ./get_news.py ./get_news.py
--- get_news.py
+++ get_news.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 import time
+import datetime
 
 service = Service('./geckodriver')
 
@@ -19,10 +20,17 @@ options.add_argument("--headless")
 driver = webdriver.Firefox(service=service, options=options)
 driver.get('https://kakoysegodnyaprazdnik.ru')
 
+current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+filename = f"./logs/file_{current_time}.txt"
+with open(filename, 'w') as file:
+    file.write("start...\n")
 try:
     element = driver.find_element(By.CLASS_NAME, "mainpage")
     page_html = driver.page_source
     print(page_html)
+    with open(filename, 'a') as file:
+        file.write("get html in first block\n")
 except:
     try:
         submit_button = WebDriverWait(driver, 600).until(
@@ -34,13 +42,18 @@ except:
         )
         page_html = driver.page_source
         print(page_html)
+        with open(filename, 'a') as file:
+            file.write("get html in second block\n")
     except:
-        print("failed found html")
+        with open(filename, 'a') as file:
+            file.write("get html FAILED\n")
 finally:
     tabs = driver.window_handles
 
     for tab in tabs:
+        with open(filename, 'a') as file:
+            file.write("close tab...\n")
         driver.switch_to.window(tab)
         driver.close()
-        
+
     driver.quit()
