b66915dbaeaea3b2d60415dfbf7085e873087bc1
paavill <pavelchist2001@gmail.com>
2024-11-07 01:52:07 +0300

    fix

--- services/geonode/service.go
+++ services/geonode/service.go
@@ -26,7 +26,7 @@ type geonode struct {
 }
 
 func (g *geonode) GetProxyList() ([]*models.Proxy, error) {
-	path := "/api/proxy-list?protocols=socks5&limit=50&page=1&sort_by=upTime&sort_type=asc"
+	path := "/api/proxy-list?protocols=socks5&limit=50&page=1&sort_by=upTime&sort_type=desc"
 	url, err := url.Parse(fmt.Sprintf("%s%s", g.host, path))
 	if err != nil {
 		return nil, err
